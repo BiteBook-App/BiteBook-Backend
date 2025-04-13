696898c74564027af1a726334e22f80358708a14
Kevin Cen <92806132+kevinc3n@users.noreply.github.com>
2025-04-13 14:00:15 -0400

    removed duplicate field

--- fast-api-backend.py
+++ fast-api-backend.py
@@ -24,7 +24,6 @@ class User:
     profilePicture: Optional[str] = None
     createdAt: Optional[str] = None
     relationships: Optional[List[str]] = None
-    relationships: Optional[List[str]] = None
 
 # Define the Recipe schema (output type for queries)
 @strawberry.type
